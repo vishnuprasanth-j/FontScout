b33b5cbf7e7cafecc230fd16fb38f03ac6b16a57
vishnuprasanth-j <jvpvis6@gmail.com>
2024-09-27 09:28:49 +0530

    final commit

--- src/utils/share.js
+++ src/utils/share.js
@@ -11,8 +11,8 @@ function Share({children, className=""}){
     const shareInfo = useMemo(() => {
 
         return {
-            url: encodeURI("https://chromewebstore.google.com/detail/FyF/deachoodakeofjlfikfkohihnpcgiaim"),
-            text: "Super charge your web fonts, Check out FyF"
+            url: encodeURI("https://chromewebstore.google.com/detail/FontScout/deachoodakeofjlfikfkohihnpcgiaim"),
+            text: "Super charge your web fonts, Check out FontScout"
         }
     }, [])
 
@@ -38,7 +38,7 @@ function Share({children, className=""}){
     return (
         <div onClick={onClick} className={className}>
             {children}
-            <Modal title={<h3 className="tw-text-xl tw-font-medium">Share FontTester</h3>} 
+            <Modal title={<h3 className="tw-text-xl tw-font-medium">Share FontScout</h3>} 
                     styles={{wrapper: {zIndex: 14000, gap: "10px"}}}
                     onCancel={onClose}
                     onOk={onClose}
